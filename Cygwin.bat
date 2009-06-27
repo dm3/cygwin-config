7069c3250c28513f720fc20099cfc61fd09a55b9
U-DEADMOPO3\Vadim <deadmoro3@gmail.com>
2009-06-27 14:38:04 +0300

    made cygwin.bat machine-agnostic (and unusable without modification)

--- Cygwin.bat
+++ Cygwin.bat
@@ -1,4 +1,5 @@
+echo SET PATH TO CYGWIN BIN (and delete me)
+rem C:
+rem chdir C:\development\tools\cygwin\bin
 @echo off
-C:
-chdir C:\development\tools\cygwin\bin
 rxvt.exe -tn rxvt-cygwin-native -sr -e bash --login -i
