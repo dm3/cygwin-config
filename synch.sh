dcc4f24fbdf53ad4312bcb268cd8a86af6c41508
dm3 <deadmoro3@gmail.com>
2011-09-07 14:54:28 +0300

    Separate branch for Cygwin configuration

--- synch.sh
+++ synch.sh
@@ -11,31 +11,15 @@ else
             cp ~/.bashrc .
             cp ~/.inputrc .
             cp ~/.ctags .
-            if [[ -n "$CYGWIN_HOME" ]]; then
-                cp ~/.Xdefaults ./.Xdefaults.CYGWIN
-                cp ~/.gitconfig ./.gitconfig.CYGWIN
-            else
-                cp ~/.xinitrc .
-                cp ~/.xmobarrc ./xmobar
-                cp ~/.xmonad/xmonad.hs ./xmonad/
-                cp ~/.gitconfig ./.gitconfig.UNIX
-                cat ~/.Xdefaults | sed "s/$dir/\$MY_SCRIPTS/" > ./.Xdefaults.UNIX
-            fi
+            cp ~/.Xdefaults ./.Xdefaults
+            cp ~/.gitconfig ./.gitconfig
         ;;
         OUT)
             cp .bashrc ~
             cp .inputrc ~
 	    cp .ctags ~
-            if [[ -n "$CYGWIN_HOME" ]]; then
-                cp .Xdefaults.CYGWIN ~/.Xdefaults
-                cp .gitconfig.CYGWIN ~/.gitconfig
-            else
-                cp .xinitrc ~
-                cp xmobar/.xmobarrc ~
-                cp xmonad/* ~/.xmonad/
-                cp .gitconfig.UNIX ~/.gitconfig
-                cat .Xdefaults.UNIX | sed "s/\$MY_SCRIPTS/$dir/" > ~/.Xdefaults
-            fi
+            cp .Xdefaults ~/.Xdefaults
+            cp .gitconfig ~/.gitconfig
         ;;
     esac
 fi
