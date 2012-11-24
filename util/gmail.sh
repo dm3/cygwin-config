3503292022fc24fe24e67bfe53b11fdce81edba7
Vadim Platonov <vadim@inventi.lt>
2012-11-24 12:21:55 +0200

    Remove unused script

--- util/gmail.sh
+++
@@ -1,7 +0,0 @@
-curl -u deadmoro3@gmail.com --silent "https://mail.google.com/mail/feed/atom" |
-  perl -ne \
-        '
-      print "Subject: $1 " if /<title>(.+?)<\/title>/ && $title++;
-          print "(from $1)\n" if /<email>(.+?)<\/email>/;
-            '
-
